d1e52c0e417c1ab3d6a7dafefea2eafb423e7344
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-28 14:25:53 -0600

    Added basic types

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // let firstValue: string = 'Ryan'
+  // let firstValue: number = 92520
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3, 56]
+  let firstValue: Array<string> = ['2','3','Ryan']
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +17,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          The value {firstValue} is of {typeof firstValue} type!
+        </p>
       </header>
     </div>
   );
