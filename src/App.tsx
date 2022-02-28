34396fda12d0d69b4bc49eb6817e1f875806567b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-28 14:46:07 -0600

    Added tuple

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
 
+  // ******* BASIC TYPES *******
   // let firstValue: string = 'Ryan'
   // let firstValue: number = 92520
   // let firstValue: boolean = true;
   // let firstValue: number[] = [2, 3, 56]
   let firstValue: Array<string> = ['2','3','Ryan']
-  
+
+  // Tuple
+  let aTuple: [string, number] = ['Ryan', 120983]
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The value {firstValue} is of {typeof firstValue} type!
         </p>
         <p>
-          The value {firstValue} is of {typeof firstValue} type!
+          The value {aTuple[0]} is of {typeof aTuple[0]} type!
         </p>
       </header>
     </div>
