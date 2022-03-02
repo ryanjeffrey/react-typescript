b0cce18b994d16989ba10cae8f2ccd42a09f0041
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-02 10:59:36 -0600

    Created interface inside component

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,12 @@ import Message from './Message.tsx'
 
 class App extends Component<any> {
 
-  componentDidMount() {
-    console.log("It worked!");
-  }
-
-
-  // // ******* BASIC TYPES *******
-  // // let firstValue: string = 'Ryan'
-  // // let firstValue: number = 92520
-  // // let firstValue: boolean = true;
-  // // let firstValue: number[] = [2, 3, 56]
-  // let firstValue: Array<string> = ['2','3','Ryan']
-
-  // // Tuple
-  // let aTuple: [string, number] = ['Ryan', 120983]
-
-  // // Enum
-  // enum Codes {first = 1, second = 2}
-
-  // // Any
-  // let firstName: any = true
-
-  // // Void
-  // const warning = (): void => {
-  //   console.log('Warning')
-  // }
-  // warning()
-
-
 render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Message message='This is a simple message'/>
+        <Message name='Ryan' message='this is a simple message.'/>
       </header>
     </div>
   );
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Message = (prop: {message: string}): any => {
+interface UserMessage {
+    name: string
+    message: string
+}
+
+const Message = (props: UserMessage): any => {
     return (
-        <p>{prop.message}</p>
+        <p>{props.name}, {props.message}</p>
     )
 }
 
